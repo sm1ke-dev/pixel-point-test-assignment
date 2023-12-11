ef94b802d49d7f0473a1d18bd139cc815e367acb
sm1ke <smike143@yandex.ru>
2023-12-11 23:02:47 +0500

    feat: создать компонент Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.scss";
+import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 
 const App: React.FC = () => {
   return (
     <div className={styles.app}>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <svg
+        className={styles.header__burger}
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        viewBox="0 0 22 22"
+        fill="none"
+      >
+        <path
+          d="M2 19L20 19"
+          stroke="#FAF9FA"
+          stroke-width="2.5"
+          stroke-linecap="round"
+        />
+        <path
+          d="M2 11L20 11"
+          stroke="#FAF9FA"
+          stroke-width="2.5"
+          stroke-linecap="round"
+        />
+        <path
+          d="M2 3L20 3"
+          stroke="#FAF9FA"
+          stroke-width="2.5"
+          stroke-linecap="round"
+        />
+      </svg>
+      <ul className={styles.header__nav}>
+        <li className={styles.header__item}>Integrations</li>
+        <li className={styles.header__item}>Core Platform</li>
+        <li className={styles.header__item}>Company</li>
+        <li className={styles.header__item}>Resources</li>
+      </ul>
+      <Link to="#" className={styles.header__link}>
+        Contact Sales
+      </Link>
+      <button className={styles.header__button}>Book a Demo</button>
+    </header>
+  );
+};
+
+export default Header;
