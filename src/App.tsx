b679da739c806e3771fbf519500aaf6f74fce6bd
sm1ke <smike143@yandex.ru>
2023-12-11 17:57:23 +0500

    feat: настроить роуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.scss";
+import Home from "./pages/Home/Home";
 
-function App() {
-  return <div className={styles.app}></div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className={styles.app}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
