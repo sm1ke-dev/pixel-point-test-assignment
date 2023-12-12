d3dc86c670edda98305a5dc651c76964bb936257
sm1ke <smike143@yandex.ru>
2023-12-12 21:11:44 +0500

    исправить верстку

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,6 +6,7 @@ import styles from "./Home.module.scss";
 const Home: React.FC = () => {
   return (
     <main className={styles.main}>
+      <div className={styles.main__background}></div>
       <Introduction />
       <Features />
     </main>
